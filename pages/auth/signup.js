1ec0c71967d4697fa44c86aa16f52b9ce058e283
Kimseonghyoek <cilo1223@naver.com>
2022-10-13 17:02:16 +0900

    Update Frontend : change css to scss, main page

---
+++ pages/auth/signup.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from '../../styles/Signup.module.scss'
+
+const Signup = () => {
+  return (
+    <div>
+      <h1 className={styles.tester}>This is Signup Page</h1>
+    </div>
+  )
+}
+
+export default Signup; 
