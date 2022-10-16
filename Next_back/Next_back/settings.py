3e5e2305a2dd69105d17149e18177ae67b2d6e46
Kimseonghyoek <cilo1223@naver.com>
2022-10-16 20:02:03 +0900

    Hide Secret Key

--- Next_back/Next_back/settings.py
+++ Next_back/Next_back/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c*mg67qvnfp-+j)^ze75!6a47t3(*tgsk$14u6(yepwx@j4hv3'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'Next_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'NEXTB',
+        'USER' : 'root',
+        'PASSWORD' : 'sad123',
+        'HOST' : 'localhost',
+        'PORT' : '3308'
     }
 }
 
