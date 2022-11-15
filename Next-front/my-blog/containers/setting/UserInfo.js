aa907dcdf5860f694ef16bacffb73a888fd7dfdb
Kimseonghyoek <cilo1223@naver.com>
2022-11-16 04:11:32 +0900

    Update UserInfo Container

--- Next-front/my-blog/containers/setting/UserInfo.js
+++ Next-front/my-blog/containers/setting/UserInfo.js
@@ -5,10 +5,50 @@ import styles from "../../styles/containers/UserInfo.module.scss";
 const UserInfo = ({ user_image, user_name, user_bio }) => {
   return (
     <div className={styles.user_info}>
-      <Image src={user_image} width={150} height={150} id={styles.user_image} />
-      <div className={styles.flex_col}>
-        <h2>{user_name}</h2>
-        <h2>{user_bio}</h2>
+      <h1>User Info</h1>
+      <div className={styles.my_info}>
+        <Image
+          src={user_image}
+          width={150}
+          height={150}
+          id={styles.user_image}
+        />
+        <div className={styles.flex_col}>
+          <h2>{user_name}</h2>
+          <h2>{user_bio}</h2>
+        </div>
+      </div>
+      <div className={styles.profile_form}>
+        <form>
+          <div className={styles.half_wrap}>
+            <div className={styles.inputs}>
+              <p>Nick Name</p>
+              <input></input>
+            </div>
+            <div className={styles.inputs}>
+              <p>Email</p>
+              <input></input>
+            </div>
+            <div className={styles.inputs}>
+              <p>Bio</p>
+              <input></input>
+            </div>
+          </div>
+          <div className={styles.half_wrap}>
+            <div className={styles.inputs}>
+              <p>Phone Number</p>
+              <input></input>
+            </div>
+            <div className={styles.inputs}>
+              <p>Password</p>
+              <input></input>
+            </div>
+            <div className={styles.inputs}>
+              <p>Location</p>
+              <input></input>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
--- Next-front/my-blog/pages/profile/setting.js
+++ Next-front/my-blog/pages/profile/setting.js
@@ -3,6 +3,7 @@ import styles from "../../styles/Setting.module.scss";
 import Header from "../../components/Navbar/Header";
 import Image from "next/image";
 import SelectSet from "../../components/Setting/SelectSet";
+import UserInfo from "../../containers/setting/UserInfo";
 
 const Setting = () => {
   return (
@@ -12,33 +13,7 @@ const Setting = () => {
         <SelectSet />
 
         <div className={styles.mainContent}>
-          <span>
-            <h1>Personal</h1>
-            <p>본인의 정보를 수정할 수 있습니다.</p>
-          </span>
-          <hr />
-          <div className={styles.forms}>
-            <div className={styles.inputs}>
-              <label>핸드폰 번호</label>
-              <input placeholder="" />
-            </div>
-            <div className={styles.inputs}>
-              <label>현재 내 직장</label>
-              <input placeholder="" />
-            </div>
-            <div className={styles.inputs}>
-              <label>Insta ID</label>
-              <input placeholder="" />
-            </div>
-            <div className={styles.inputs}>
-              <label>내 커리어</label>
-              <input placeholder="" />
-            </div>
-            <div className={styles.inputs}>
-              <label>성별</label>
-              <input placeholder="" />
-            </div>
-          </div>
+          <UserInfo/>
         </div>
       </div>
     </div>
