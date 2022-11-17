914f492f5c20ece111524f5be1ee00d50b6d003d
Kimseonghyoek <cilo1223@naver.com>
2022-11-18 05:16:39 +0900

    Update test page and Create test styles

--- Next-front/my-blog/pages/test.js
+++ Next-front/my-blog/pages/test.js
@@ -1,13 +1,17 @@
 import React from "react";
 import SelectSet from "../components/Setting/SelectSet";
+import styles from "../styles/test.module.scss";
 import UserInfo from "../containers/setting/UserInfo";
 
 const Test = () => {
   return (
-    <>
-      <SelectSet/>
-    </>
-  )
-}
+    <html className={styles.test}>
+      <div>
+        {/* <SelectSet /> */}
+        <UserInfo/>
+      </div>
+    </html>
+  );
+};
 
-export default Test
+export default Test;
