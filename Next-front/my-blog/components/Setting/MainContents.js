2ab4f409f579f7f2ddcd0198e34797f9ab6235d3
Kimseonghyoek <cilo1223@naver.com>
2022-11-21 17:14:46 +0900

    Update MainContents Components

--- Next-front/my-blog/components/Setting/MainContents.js
+++ Next-front/my-blog/components/Setting/MainContents.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import SettingCom from '../../containers/setting/SettingCom';
+import UserInfo from '../../containers/setting/UserInfo';
+import { setting_item } from '../../recoil/store'; 
 
 const MainContent = () => {
-  return (
-    <div></div>
-  )
-}
+  
+  const [set_item, setItem] = useRecoilState(setting_item);
+  
+  if(set_item === 0) {
+    return (
+      <>
+        <UserInfo/>
+      </>
+    )
+  } else if (set_item === 1) {
+    return (
+      <>
+        <SettingCom/>
+      </>
+    )
+  } else if (set_item === 2) {
+    return (
+      <>
+        {/* Like Post */}
+      </>
+    )
+  }
+} 
 
 export default MainContent;
