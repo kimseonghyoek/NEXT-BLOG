ecde537c96d38142e332110bd91aed7086e40dac
Kimseonghyoek <cilo1223@naver.com>
2022-11-22 05:09:16 +0900

    Create Setting & LikePost Components

---
+++ Next-front/my-blog/components/Setting/LikePost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LikePost = () => {
+  return (
+    <div>
+      <h1>This is Like Post</h1>
+    </div>
+  )
+}
+
+export default LikePost;
---
+++ Next-front/my-blog/containers/setting/SettingCom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SettingCom = () => {
+  return (
+    <div>
+      <h1>This is setting</h1>
+    </div>
+  )
+}
+
+export default SettingCom;
