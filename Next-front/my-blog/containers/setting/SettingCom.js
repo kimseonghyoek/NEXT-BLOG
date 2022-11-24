8f4e60a616a5db65a127d424675a7e8f5f12d33d
Kimseonghyoek <cilo1223@naver.com>
2022-11-24 20:44:00 +0900

    Update Setting Components

--- Next-front/my-blog/containers/setting/SettingCom.js
+++ Next-front/my-blog/containers/setting/SettingCom.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import ToggleBar from "../../components/DefaultComponents/ToggleBar";
+import styles from "../../styles/containers/SettingCom.module.scss";
+import DelAccount from "../DelAccount";
 
 const SettingCom = () => {
   return (
     <div>
-      <h1>This is setting</h1>
+      <h1>Setting</h1>
+      <div className={styles.contents}>
+        <h2>Display Setting</h2>
+        <div>
+          <span className={styles.tags}>Dark Mode</span>
+          <ToggleBar />
+        </div>
+        <div>
+          <h2>Account Setting</h2>
+          <div>
+            <DelAccount />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SettingCom;
