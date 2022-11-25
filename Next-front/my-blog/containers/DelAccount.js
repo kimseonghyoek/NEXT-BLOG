d424d6cbbc734ee17a1badfbed1cd4b97eab91a3
Kimseonghyoek <cilo1223@naver.com>
2022-11-25 16:25:36 +0900

    Update DelAccount Container

--- Next-front/my-blog/containers/DelAccount.js
+++ Next-front/my-blog/containers/DelAccount.js
@@ -3,13 +3,31 @@ import styles from "../styles/containers/DekAccount.module.scss";
 import { useRecoilState } from "recoil";
 import { email } from "../recoil/store";
 
-const DelAccount = () => {
+const DelAccount = ({ mail }) => {
+  // 실제 배포 및 포폴 공유시 admin 이메일 가져와서 보여주기
   const user_email = useRecoilState(email);
   const [del_account, setShowDel] = useState(false);
+  const [input_state, setInput] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   const delUserAccount = () => {
     setShowDel(!del_account);
+    setDisabled(true);
   };
+
+  const changeForm = (e) => {
+    e.preventDefault();
+    setInput(e.target.value);
+  }
+
+  const checkVaild = () => {
+    mail === input_state ? setDisabled(false) : setDisabled(true);
+  }
+  
+  const submit = () => {
+    alert("계정이 삭제되었습니다.")
+  }
+
   return (
     <div className={styles.del_account}>
       <div>
@@ -27,9 +45,11 @@ const DelAccount = () => {
             계정을 삭제하실려면 이메일을 입력하세요.
           </p>
           <div className={styles.inputs}>
-            <h3>TEST EMAIL</h3>
-            <input placeholder="Edit your email"></input>
-            <button>Submit</button>
+            <h3>{ mail }</h3>
+            <div className={styles.forms}>
+              <input placeholder="Edit your email" onChange={changeForm} onKeyUp={checkVaild}></input>
+              <button onClick={submit} disabled={disabled}>Submit</button>
+            </div>
           </div>
         </div>
       ) : null}
