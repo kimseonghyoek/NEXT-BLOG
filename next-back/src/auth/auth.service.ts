e60bd9a2cedc32afb16d1b7553967bfbe8103786
Kimseonghyoek <cilo1223@naver.com>
2022-11-28 03:29:17 +0900

    Update Auth Service Logic

--- next-back/src/auth/auth.service.ts
+++ next-back/src/auth/auth.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { response } from 'express';
+// import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -10,15 +12,27 @@ export class AuthService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
+  public async checkEmail(body) {
+    const user = await this.userRepository.findOne({
+      where: { email: body },
+    });
+    return user !== undefined;
+  }
+
   // 나중에 타입 지정
   public async signUp(body) {
     const user = new User();
-    console.log('====log====');
     user.id = body.body.id;
     user.email = body.body.email;
+    const checkEmail = await this.checkEmail(body.body.email);
+    if (checkEmail) {
+      console.log('이메일 중복이다');
+    }
     user.birth = body.body.birth;
     user.nickname = body.body.nickname;
-    user.pw = body.body.pw;
+    const password = body.body.pw;
+    const salt = 12;
+    user.pw = await bcrypt.hash(password, salt);
     await this.userRepository.save(user);
   }
 }
