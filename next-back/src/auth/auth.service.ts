96252718a5baab14e60f2078ed12f4073ef3f028
Kimseonghyoek <cilo1223@naver.com>
2022-12-18 19:39:19 +0900

    Update Auth Service

--- next-back/src/auth/auth.service.ts
+++ next-back/src/auth/auth.service.ts
@@ -7,7 +7,6 @@ import {
   InternalServerErrorException,
   UnauthorizedException,
 } from "@nestjs/common/exceptions";
-import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class AuthService {
@@ -15,50 +14,11 @@ export class AuthService {
     @InjectRepository(User) private userRepository: Repository<User>
   ) {}
 
-  public async checkEmail(email: string) {
-    const use_email = await this.userRepository.findOne({
-      where: { email: email },
-    });
-
-    if (use_email) {
-      throw new UnauthorizedException("use_this_email");
-    }
-  }
-
-  public async checkNickname(name: string) {
-    const use_name = await this.userRepository.findOne({
-      where: { nickname: name },
-    });
-
-    if (use_name) {
-      throw new UnauthorizedException("use_this_nickname");
-    }
-  }
-
   public async findAll() {
     return await this.userRepository.find();
   }
 
-  public async createUser(createUserDto: CreateUserDto) {
-    const { userEmail, userBirth, userName, userPw } = createUserDto;
-    await this.checkEmail(userEmail);
-    await this.checkNickname(userName);
-    const password = await bcrypt.hash(userPw, 12);
-
-    try {
-      await this.userRepository.save({
-        email: userEmail,
-        birth: userBirth,
-        nickname: userName,
-        pw: password,
-      });
-    } catch (err) {
-      console.log(err);
-      throw new InternalServerErrorException();
-    }
-  }
-
-  public async login(email: string, pw: string) {
+  public async validateLogin(email: string, pw: string) {
     const user = await this.userRepository.findOne({
       where: { email: email },
     });
@@ -67,9 +27,11 @@ export class AuthService {
       const match = await bcrypt.compare(pw, user.pw);
       if (match) {
         console.log(match);
-      } else {
-        throw new UnauthorizedException("fail_login");
+      } else if (!match) {
+        throw new UnauthorizedException("NOT_MATCH_PW");
       }
+    } else if (user === null) {
+      throw new UnauthorizedException("NOT_FOUND_EMAIL");
     }
 
     return user;
