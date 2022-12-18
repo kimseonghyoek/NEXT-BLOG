ca2a33df434188e691f12d185aa1d1c6b1d79439
Kimseonghyoek <cilo1223@naver.com>
2022-12-18 19:04:16 +0900

    Update InputCheck Components

--- Next-front/my-blog/components/InputCheck.js
+++ Next-front/my-blog/components/InputCheck.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
+import { check_input } from '../recoil/store';
 import style from '../styles/components/common/InputCheck.module.scss';
 
 const InputCheck = ({ notice }) => {
+
+  const [show_notice] = useRecoilState(check_input);
+
   return (
     <div className={style.input_check}>
       <p>{notice}</p>
