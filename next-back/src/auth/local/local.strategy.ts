f08a2dde4707d0fcfddbe587f498f69323aa7a37
Kimseonghyoek <cilo1223@naver.com>
2022-12-25 23:39:58 +0900

    Create Local Strategy

---
+++ next-back/src/auth/local/local.strategy.ts
@@ -0,0 +1,25 @@
+import { UnauthorizedException } from "@nestjs/common";
+import { Body, Injectable } from "@nestjs/common/decorators";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt } from "passport-jwt";
+import { Strategy } from "passport-local";
+import { AuthService } from "../auth.service";
+import { Local } from "./local.interface";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: "userEmail",
+      passwordField: "userPw",
+    });
+  }
+
+  async validate(email: string, pw: string): Promise<any> {
+    const user = await this.authService.validateLogin(email, pw);
+    if (!user) {
+      throw new UnauthorizedException("local error");
+    }
+    return user;
+  }
+}
