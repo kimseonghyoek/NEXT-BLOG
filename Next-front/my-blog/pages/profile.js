a61f27a339e48f28db298c3536c9f4a121ac5709
Kimseonghyoek <cilo1223@naver.com>
2022-12-28 23:03:13 +0900

    Create Profile Page

---
+++ Next-front/my-blog/pages/profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = ({ props }) => {
+  return (
+    <div>
+      <h1>This is Profile page</h1>
+      <div>
+        <h2>User Info</h2>
+        {
+          
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
